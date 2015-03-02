73cadcfad71655cefed4edb7fe3487b56a43a32f
zsx <thinkernel@gmail.com>
2015-03-02 11:49:02 +0800

    Add environment parameters for database

--- gerrit-entrypoint.sh
+++ gerrit-entrypoint.sh
@@ -12,8 +12,15 @@ if [ "$1" = '/var/gerrit/gerrit-start.sh' ]; then
       echo "Something wrong..."
       cat $GERRIT_SITE/logs/error_log
     fi
-    #Customize gerrit.config
-    [ -z $CANONICAL_WEBURL ] || git config -f $GERRIT_SITE/etc/gerrit.config gerrit.canonicalWebUrl $CANONICAL_WEBURL
   fi
+  #Customize gerrit.config
+  #Section gerrit
+  [ -z $CANONICAL_WEBURL ] || git config -f $GERRIT_SITE/etc/gerrit.config gerrit.canonicalWebUrl $CANONICAL_WEBURL
+  #Section database
+  [ -z $DATABASE_TYPE ] || git config -f $GERRIT_SITE/etc/gerrit.config database.type $DATABASE_TYPE
+  [ -z $DATABASE_HOSTNAME ] || git config -f $GERRIT_SITE/etc/gerrit.config database.hostname $DATABASE_HOSTNAME
+  [ -z $DATABASE_DATABASE ] || git config -f $GERRIT_SITE/etc/gerrit.config database.database $DATABASE_DATABASE
+  [ -z $DATABASE_USERNAME ] || git config -f $GERRIT_SITE/etc/secure.config database.username $DATABASE_USERNAME
+  [ -z $DATABASE_PASSWORD ] || git config -f $GERRIT_SITE/etc/secure.config database.password $DATABASE_PASSWORD
 fi
 exec "$@"
