572cd10328377e59b25102b58698d8f39f4f468d
thinkernel <thinkernel@gmail.com>
2016-02-07 16:16:06 +0800

    Set sendemail.enable=true when smtp server is provided.

--- gerrit-entrypoint.sh
+++ gerrit-entrypoint.sh
@@ -103,6 +103,7 @@ if [ "$1" = "/gerrit-start.sh" ]; then
   if [ -z "${SMTP_SERVER}" ]; then
     set_gerrit_config sendemail.enable false
   else
+    set_gerrit_config sendemail.enable true
     set_gerrit_config sendemail.smtpServer "${SMTP_SERVER}"
     if [ "smtp.gmail.com" = "${SMTP_SERVER}" ]; then
       echo "gmail detected, using default port and encryption"
