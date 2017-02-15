3fa9cb8884b848b4a79ff7578358f2c086fdf4fc
Joan <joan@goyeau.com>
2017-02-15 12:01:59 +0000

    Fix gitweb not shown and support gitiles

--- gerrit-entrypoint.sh
+++ gerrit-entrypoint.sh
@@ -168,7 +168,14 @@ if [ "$1" = "/gerrit-start.sh" ]; then
   [ -z "${HTTPD_LISTENURL}" ] || set_gerrit_config httpd.listenUrl "${HTTPD_LISTENURL}"
 
   #Section gitweb
-  set_gerrit_config gitweb.cgi "/usr/share/gitweb/gitweb.cgi"
+  case "$GITWEB_TYPE" in
+     "gitiles") su-exec $GERRIT_USER cp -f $GERRIT_HOME/gitiles.jar $GERRIT_SITE/plugins/gitiles.jar ;;
+     "") # Gitweb by default
+        set_gerrit_config gitweb.cgi "/usr/share/gitweb/gitweb.cgi"
+        export GITWEB_TYPE=gitweb
+     ;;
+  esac
+  set_gerrit_config gitweb.type "$GITWEB_TYPE"
 
   echo "Upgrading gerrit..."
   su-exec ${GERRIT_USER} java ${JAVA_OPTIONS} ${JAVA_MEM_OPTIONS} -jar "${GERRIT_WAR}" init --batch -d "${GERRIT_SITE}" ${GERRIT_INIT_ARGS}
